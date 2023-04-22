636bc95c78cf2f889aa6f00bddb80a318b8df6a2
Попов Мирон Ильич <ip111s30@csc.sibsutis.ru>
2023-04-22 11:35:31 +0700

    я двоешник

--- src/lib/myBigChars.c
+++ src/lib/myBigChars.c
@@ -16,9 +16,8 @@ unsigned int bc[][2] = {
 /* 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,+,- */
 
 int
-bc_printA (char ch)
-{
-  printf ("\033(0%c\033(B", ch);
+bc_printA (char ch){
+  write (1, &ch, sizeof (ch));
   return 0;
 }
 int
